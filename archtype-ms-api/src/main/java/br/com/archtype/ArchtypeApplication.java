1ebba0300ec8555b72a28a5f1a0477300bffa212
Adilson Rodrigues da Silva <adilson.ro.silva@gmail.com>
2021-09-04 13:30:07 -0300

    Add files via upload

---
+++ archtype-ms-api/src/main/java/br/com/archtype/ArchtypeApplication.java
@@ -0,0 +1,13 @@
+package br.com.archtype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchtypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchtypeApplication.class, args);
+	}
+
+}
---
+++ archtype-ms-api/src/test/java/br/com/archtype/ArchtypeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.archtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchtypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
